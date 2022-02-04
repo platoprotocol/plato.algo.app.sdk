71c5fd8b77017aca6eb049fbae459c0377bc2571
Alexey Chernenko <alexey@platoprotocol.com>
2022-02-04 12:17:08 +0200

    refactor: merge params into an object

--- src/algo/AlgoAppManager.ts
+++ src/algo/AlgoAppManager.ts
@@ -9,27 +9,27 @@ export default class AlgoAppManager {
    * Compiles and deploys the provided stateful application
    * @returns An ID of the newly created application
    */
-  async create(
-    creatorMnemonic: string,
-    approvalProgramSource: string,
-    clearProgramSource: string,
-    localInts: number,
-    localBytes: number,
-    globalInts: number,
-    globalBytes: number,
-    appArgs?: AppArgument[],
-    accounts?: string[],
-    foreignApps?: number[],
-    foreignAssets?: number[],
-    note?: AppArgument,
-    lease?: AppArgument,
-    rekeyTo?: string,
-    extraPages?: number
-  ): Promise<{ id: number; address: string }> {
-    const senderAccount = algosdk.mnemonicToSecretKey(creatorMnemonic);
+  async create(args: {
+    creatorMnemonic: string;
+    approvalProgramSource: string;
+    clearProgramSource: string;
+    localInts: number;
+    localBytes: number;
+    globalInts: number;
+    globalBytes: number;
+    appArgs?: AppArgument[];
+    accounts?: string[];
+    foreignApps?: number[];
+    foreignAssets?: number[];
+    note?: AppArgument;
+    lease?: AppArgument;
+    rekeyTo?: string;
+    extraPages?: number;
+  }): Promise<{ id: number; address: string }> {
+    const senderAccount = algosdk.mnemonicToSecretKey(args.creatorMnemonic);
     const [approvalProgram, clearProgram, params] = await Promise.all([
-      this.algoClient.compileProgram(approvalProgramSource),
-      this.algoClient.compileProgram(clearProgramSource),
+      this.algoClient.compileProgram(args.approvalProgramSource),
+      this.algoClient.compileProgram(args.clearProgramSource),
       this.algoClient.getDefaultParams(),
     ]);
     const txn = algosdk.makeApplicationCreateTxn(
@@ -38,18 +38,18 @@ export default class AlgoAppManager {
       algosdk.OnApplicationComplete.NoOpOC,
       approvalProgram,
       clearProgram,
-      localInts,
-      localBytes,
-      globalInts,
-      globalBytes,
-      appArgs?.map((appArg) => appArg.toBinary()),
-      accounts,
-      foreignApps,
-      foreignAssets,
-      note?.toBinary(),
-      lease?.toBinary(),
-      rekeyTo,
-      extraPages
+      args.localInts,
+      args.localBytes,
+      args.globalInts,
+      args.globalBytes,
+      args.appArgs?.map((appArg) => appArg.toBinary()),
+      args.accounts,
+      args.foreignApps,
+      args.foreignAssets,
+      args.note?.toBinary(),
+      args.lease?.toBinary(),
+      args.rekeyTo,
+      args.extraPages
     );
     await this.algoClient.sendRawTransaction(txn, senderAccount.sk);
     const appId = await this.algoClient.getApplicationId(txn);
@@ -62,80 +62,71 @@ export default class AlgoAppManager {
   /**
    * Updates an application's approval and clear programs
    */
-  async update(
-    creatorMnemonic: string,
-    appId: number,
-    approvalProgramSource: string,
-    clearProgramSource: string,
-    appArgs?: AppArgument[],
-    accounts?: string[],
-    foreignApps?: number[],
-    foreignAssets?: number[],
-    note?: AppArgument,
-    lease?: AppArgument,
-    rekeyTo?: string
-  ): Promise<void> {
+  async update(args: {
+    creatorMnemonic: string;
+    appId: number;
+    approvalProgramSource: string;
+    clearProgramSource: string;
+    appArgs?: AppArgument[];
+    accounts?: string[];
+    foreignApps?: number[];
+    foreignAssets?: number[];
+    note?: AppArgument;
+    lease?: AppArgument;
+    rekeyTo?: string;
+  }): Promise<void> {
     const [approvalProgram, clearProgram] = await Promise.all([
-      this.algoClient.compileProgram(approvalProgramSource),
-      this.algoClient.compileProgram(clearProgramSource),
+      this.algoClient.compileProgram(args.approvalProgramSource),
+      this.algoClient.compileProgram(args.clearProgramSource),
     ]);
     await this.algoClient.sendTransaction(
-      creatorMnemonic,
+      args.creatorMnemonic,
       (fromAddress, params) =>
         algosdk.makeApplicationUpdateTxn(
           fromAddress,
           params,
-          appId,
+          args.appId,
           approvalProgram,
           clearProgram,
-          appArgs?.map((appArg) => appArg.toBinary()),
-          accounts,
-          foreignApps,
-          foreignAssets,
-          note?.toBinary(),
-          lease?.toBinary(),
-          rekeyTo
+          args.appArgs?.map((appArg) => appArg.toBinary()),
+          args.accounts,
+          args.foreignApps,
+          args.foreignAssets,
+          args.note?.toBinary(),
+          args.lease?.toBinary(),
+          args.rekeyTo
         )
     );
   }
 
   /**
    * Calls an app with the provided set of parameters
-   * @param senderMnemonic
-   * @param appId
-   * @param appArgs
-   * @param accounts
-   * @param foreignApps
-   * @param foreignAssets
-   * @param note
-   * @param lease
-   * @param rekeyTo
    */
-  async invoke(
-    senderMnemonic: string,
-    appId: number,
-    appArgs?: AppArgument[],
-    accounts?: string[],
-    foreignApps?: number[],
-    foreignAssets?: number[],
-    note?: AppArgument,
-    lease?: AppArgument,
-    rekeyTo?: string
-  ): Promise<void> {
+  async invoke(args: {
+    senderMnemonic: string;
+    appId: number;
+    appArgs?: AppArgument[];
+    accounts?: string[];
+    foreignApps?: number[];
+    foreignAssets?: number[];
+    note?: AppArgument;
+    lease?: AppArgument;
+    rekeyTo?: string;
+  }): Promise<void> {
     await this.algoClient.sendTransaction(
-      senderMnemonic,
+      args.senderMnemonic,
       (fromAddress, params) =>
         algosdk.makeApplicationNoOpTxn(
           fromAddress,
           params,
-          appId,
-          appArgs?.map((appArg) => appArg.toBinary()),
-          accounts,
-          foreignApps,
-          foreignAssets,
-          note?.toBinary(),
-          lease?.toBinary(),
-          rekeyTo
+          args.appId,
+          args.appArgs?.map((appArg) => appArg.toBinary()),
+          args.accounts,
+          args.foreignApps,
+          args.foreignAssets,
+          args.note?.toBinary(),
+          args.lease?.toBinary(),
+          args.rekeyTo
         )
     );
   }
@@ -143,31 +134,31 @@ export default class AlgoAppManager {
   /**
    * Opts in to use an application
    */
-  async optIn(
-    senderMnemonic: string,
-    appId: number,
-    appArgs?: AppArgument[],
-    accounts?: string[],
-    foreignApps?: number[],
-    foreignAssets?: number[],
-    note?: AppArgument,
-    lease?: AppArgument,
-    rekeyTo?: string
-  ): Promise<void> {
+  async optIn(args: {
+    senderMnemonic: string;
+    appId: number;
+    appArgs?: AppArgument[];
+    accounts?: string[];
+    foreignApps?: number[];
+    foreignAssets?: number[];
+    note?: AppArgument;
+    lease?: AppArgument;
+    rekeyTo?: string;
+  }): Promise<void> {
     await this.algoClient.sendTransaction(
-      senderMnemonic,
+      args.senderMnemonic,
       (fromAddress, params) =>
         algosdk.makeApplicationOptInTxn(
           fromAddress,
           params,
-          appId,
-          appArgs?.map((appArg) => appArg.toBinary()),
-          accounts,
-          foreignApps,
-          foreignAssets,
-          note?.toBinary(),
-          lease?.toBinary(),
-          rekeyTo
+          args.appId,
+          args.appArgs?.map((appArg) => appArg.toBinary()),
+          args.accounts,
+          args.foreignApps,
+          args.foreignAssets,
+          args.note?.toBinary(),
+          args.lease?.toBinary(),
+          args.rekeyTo
         )
     );
   }
@@ -175,31 +166,31 @@ export default class AlgoAppManager {
   /**
    * Make a transaction that deletes an application
    */
-  async delete(
-    senderMnemonic: string,
-    appId: number,
-    appArgs?: AppArgument[],
-    accounts?: string[],
-    foreignApps?: number[],
-    foreignAssets?: number[],
-    note?: AppArgument,
-    lease?: AppArgument,
-    rekeyTo?: string
-  ): Promise<void> {
+  async delete(args: {
+    senderMnemonic: string;
+    appId: number;
+    appArgs?: AppArgument[];
+    accounts?: string[];
+    foreignApps?: number[];
+    foreignAssets?: number[];
+    note?: AppArgument;
+    lease?: AppArgument;
+    rekeyTo?: string;
+  }): Promise<void> {
     await this.algoClient.sendTransaction(
-      senderMnemonic,
+      args.senderMnemonic,
       (fromAddress, params) =>
         algosdk.makeApplicationDeleteTxn(
           fromAddress,
           params,
-          appId,
-          appArgs?.map((appArg) => appArg.toBinary()),
-          accounts,
-          foreignApps,
-          foreignAssets,
-          note?.toBinary(),
-          lease?.toBinary(),
-          rekeyTo
+          args.appId,
+          args.appArgs?.map((appArg) => appArg.toBinary()),
+          args.accounts,
+          args.foreignApps,
+          args.foreignAssets,
+          args.note?.toBinary(),
+          args.lease?.toBinary(),
+          args.rekeyTo
         )
     );
   }
