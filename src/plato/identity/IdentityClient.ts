a84ff94f5cfea7b7d07a3b732e71476cb0f006f1
Alexey Chernenko <alexey@platoprotocol.com>
2022-02-17 16:25:32 +0200

    refactor: identity client use get future time func

--- src/plato/identity/IdentityClient.ts
+++ src/plato/identity/IdentityClient.ts
@@ -7,7 +7,7 @@ import AddressAppArgument from "../../algo/types/app/arguments/AddressAppArgumen
 import NumberAppArgument from "../../algo/types/app/arguments/NumberAppArgument";
 import StringAppArgument from "../../algo/types/app/arguments/StringAppArgument";
 import { StateSchema } from "../../algo/types/app/types";
-import { unixNow } from "../../utils/date";
+import { getFutureTime } from "../../utils/date";
 import {
   IdentityActionType,
   IDENTITY_VALIDATE_COURIER,
@@ -37,7 +37,7 @@ export default class IdentityClient {
     ]);
     const localState: StateSchema = { ints: 8, bytes: 8 };
     const globalState: StateSchema = { ints: 1, bytes: 1 };
-    const startTime = IdentityClient.getStartTime();
+    const startTime = getFutureTime();
     const appArgs = [new NumberAppArgument(startTime)];
     const { id } = await algoAppManager.create({
       creatorMnemonic,
@@ -58,7 +58,7 @@ export default class IdentityClient {
     longitude: string,
     refererAddress: string = ALGORAND_ZERO_ADDRESS_STRING
   ): Promise<void> {
-    const startTime = IdentityClient.getStartTime();
+    const startTime = getFutureTime();
     const { addr: userAddress } = mnemonicToSecretKey(userMnemonic);
     await this.algoAppManager.optIn({
       senderMnemonic: userMnemonic,
@@ -80,7 +80,7 @@ export default class IdentityClient {
   ): Promise<void> {
     const actionType: IdentityActionType = IDENTITY_VALIDATE_COURIER;
     const userType: UserType = USER_TYPE_BUYER;
-    const startTime = IdentityClient.getStartTime();
+    const startTime = getFutureTime();
     await this.algoAppManager.invoke({
       senderMnemonic: merchantMnemonic,
       appId: this.appId,
@@ -99,7 +99,7 @@ export default class IdentityClient {
   ): Promise<void> {
     const actionType: IdentityActionType = IDENTITY_VALIDATE_MERCHANT;
     const userType: UserType = USER_TYPE_BUYER;
-    const startTime = IdentityClient.getStartTime();
+    const startTime = getFutureTime();
     await this.algoAppManager.invoke({
       senderMnemonic: buyerMnemonic,
       appId: this.appId,
@@ -110,12 +110,5 @@ export default class IdentityClient {
         new AddressAppArgument(merchantAddress),
       ],
     });
-  }
-
-  /**
-   * Return the start time which is 10 seconds in the future.
-   */
-  private static getStartTime(): number {
-    return unixNow() + 10;
   }
 }
